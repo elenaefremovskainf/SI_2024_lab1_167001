530544009f6eeff4bc5d81be3d53cc43a9c765ee
Elena <elena.prog.codelabexplorer@gmail.com>
2024-04-14 16:10:36 +0200

    Fix wrong condition in makeBigStringForm

--- SILab1_2.java
+++ SILab1_2.java
@@ -5,18 +5,10 @@ public class SILab1_2 {
     private static String makeBigStringFrom (List<String> list, int minLength) {
         StringBuilder sb = new StringBuilder();
         for (String el : list) {
-<<<<<<< HEAD
-            if (el.length() < minLength) {
-=======
             if (el.length() >= minLength) {
->>>>>>> 2b79542e1ef7a7774ea110d94ca17935770823cb
                 sb.append(el);
             }
         }
         return sb.toString();
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 2b79542e1ef7a7774ea110d94ca17935770823cb
